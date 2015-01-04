345b10c4d11c5984130cc78ee58bdc482857c147
Elia <elia.bruni@gmail.com>
2015-01-04 13:44:10 +0100

    progress on profile view

--- www/js/tab-account.ctrl.js
+++ www/js/tab-account.ctrl.js
@@ -1,17 +1,18 @@
 angular.module('splash.tabAccount.ctrl', [])
 
 
-    .controller('TabAccountCtrl', function($scope, $state, $ionicModal, $ionicLoading, $ionicPopup, $cordovaFacebook, $cordovaFile, $cordovaGeolocation, $localstorage, Geo) {
+    .controller('TabAccountCtrl', function($scope, $q, $state, $ionicModal, $ionicLoading,
+                                           $ionicPopup, $cordovaFacebook, $cordovaFile,
+                                           $cordovaGeolocation, $localstorage, Geo) {
 
         $scope.showView = true;
 
-        if ($localstorage.getObject('myCity') == null) {
+        if ($localstorage.getObject('myCity') == null || $localstorage.getObject('profilePhoto') == null) {
             $scope.showView = false;
 
             $ionicLoading.show({
                 template: 'loading'
             });
-
 
             // Wait for device API libraries to load
             document.addEventListener("deviceready", onDeviceReady, false);
@@ -19,53 +20,28 @@ angular.module('splash.tabAccount.ctrl', [])
             // device APIs are available
             function onDeviceReady() {
 
-                $cordovaFacebook.api("me/picture?redirect=0&height=400&type=normal&width=400", ["public_profile"])
-                    .then(function (success) {
-                        var photo = success.data;
-                        var fileTransferDir = cordova.file.externalDataDirectory;
+                $scope.profilePhotoReady = false;
+                $scope.profileInfoReady = false;
 
-                        var hostPath = photo.url;
-                        var clientPath = fileTransferDir + 'test.jpg';
-                        var fileTransferOptions = {};
+                $scope.$watchGroup(['profilePhotoReady', 'profileInfoReady'], function(newValues, oldValues) {
+                    if (newValues[0] && newValues[1]) {
+                        $ionicLoading.hide();
+                        $scope.showView = true;
+                    }
+                });
 
-                        $cordovaFile.downloadFile(hostPath, clientPath, true, fileTransferOptions).then (function(result) {
-                            // Success!
-                            alert('image download success!')
-                            $scope.profilePhoto = clientPath;
-                            $localstorage.set('profilePhoto', $scope.profilePhoto)
-
-                        }, function(err) {
-                            // Error
-                            //alert(err)
-                        }, function (progress) {
-                            // constant progress updates
-                        });
-
+                getProfilePhoto()
+                    .then(function(success) {
+                        $scope.profilePhotoReady = true;
                     }, function (error) {
-                        alert('error')
-
-                        console.log(error);
+                        // error
                     });
 
-
-                $cordovaGeolocation
-                    .getCurrentPosition({timeout: 10000, enableHighAccuracy: true})
-                    .then(function (position) {
-
-
-                        Geo.facebookGeoLocation(position.coords.latitude, position.coords.longitude, 50000, function (cities) {
-                            $scope.cities = cities;
-                            $scope.myCity = cities[0];
-                            $ionicLoading.hide();
-                            $scope.showView = true;
-                            $localstorage.setObject('myCity', cities[0]);
-                            $localstorage.setObject('cities', cities);
-                        });
-
-                    }, function (err) {
-                        console.log("unable to find location");
-                        $scope.errorMsg = "Error : " + err.message;
-                        alert($scope.errorMsg)
+                getCities()
+                    .then(function(success) {
+                        $scope.profileInfoReady = true;
+                    }, function (error) {
+                        // error
                     });
             }
         } else {
@@ -75,7 +51,6 @@ angular.module('splash.tabAccount.ctrl', [])
 
         }
 
-
         // set localStorage when function is called after a value is changed
         $scope.updateStorage = function(city){
 
@@ -83,5 +58,66 @@ angular.module('splash.tabAccount.ctrl', [])
             $localstorage.setObject('myCity', city);
             //$scope.myCity = $localstorage.getObject('myCity');
         };
+
+        /***********************************************
+         *               HELPER FUNCTIONS              *
+         ***********************************************/
+
+        function getProfilePhoto() {
+
+            var d = $q.defer();
+
+            $cordovaFacebook.api("me/picture?redirect=0&height=400&type=normal&width=400", ["public_profile"])
+                .then(function (success) {
+
+                    var photo = success.data;
+                    var fileTransferDir = cordova.file.externalDataDirectory;
+
+                    var hostPath = photo.url;
+                    var clientPath = fileTransferDir + 'test.jpg';
+                    var fileTransferOptions = {};
+
+                    $cordovaFile.downloadFile(hostPath, clientPath, true, fileTransferOptions).then (function(result) {
+                        // Success!
+                        $scope.profilePhoto = clientPath;
+                        $localstorage.set('profilePhoto', $scope.profilePhoto);
+                        d.resolve();
+
+                    }, function(err) {
+                        // Error
+                        d.reject();
+                    }, function (progress) {
+                        // constant progress updates
+                    });
+
+                }, function (error) {
+                    d.reject();
+                });
+
+            return d.promise;
+        }
+
+        function getCities() {
+
+            var d = $q.defer();
+
+            $cordovaGeolocation
+                .getCurrentPosition({timeout: 10000, enableHighAccuracy: true})
+                .then(function (position) {
+
+                    Geo.facebookGeoLocation(position.coords.latitude, position.coords.longitude, 50000, function (cities) {
+                        $scope.cities = cities;
+                        $scope.myCity = cities[0];
+                        $localstorage.setObject('myCity', cities[0]);
+                        $localstorage.setObject('cities', cities);
+                        d.resolve();
+                    });
+
+                }, function (err) {
+                    d.reject();
+
+                });
+            return d.promise;
+        }
 
     })
